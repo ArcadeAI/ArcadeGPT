d8e4554037240079fb9b639613d448a379776560
Sergio Serrano <sdserranog@gmail.com>
2024-11-15 20:27:16 -0400

    Update dependencies to stable versions

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: true,
-  },
   images: {
-    remotePatterns: [
-    ],
+    remotePatterns: [],
   },
 };
 
